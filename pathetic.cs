7e87286ba6b9497f0b10c92f86b86bbd64a420e0
Алёна Чистякова <113229355+aspohdl@users.noreply.github.com>
2023-02-12 16:17:38 +0300

    Add files via upload

---
+++ pathetic.cs
@@ -0,0 +1,23 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+if (number > 999)
+{
+    if (number % 10 == number / 1000)
+    {
+        if (number % 100 / 10 == number % 1000 / 100)
+        {
+            Console.WriteLine("True");
+        }
+        else if (number % 100 / 10 == number % 1000 / 100)
+        {
+            Console.WriteLine("False");
+        }
+    }
+    else if (number % 10 != number / 1000)
+    {
+        Console.WriteLine("False");
+    }
+}
+else if (number < 1000)
+{
+    Console.WriteLine("False");
+}
