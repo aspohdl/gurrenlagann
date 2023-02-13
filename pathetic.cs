ad9e906e29744d4502830390a3ff62829f604b5e
Алёна Чистякова <113229355+aspohdl@users.noreply.github.com>
2023-02-13 21:11:46 +0300

    Add files via upload

---
+++ pathetic.cs
@@ -0,0 +1,25 @@
+﻿double x = Convert.ToDouble(Console.ReadLine());
+double y = Convert.ToDouble(Console.ReadLine());
+int square_root = 2;
+if (y > 0)
+{
+    if ((Math.Pow(x, square_root) + Math.Pow(y, square_root)) <= 4)
+    {
+        if ((Math.Pow(x, square_root) + Math.Pow(y, square_root)) >= 1)
+        {
+            Console.WriteLine("1");
+        }
+        else
+        {
+            Console.WriteLine("0");
+        }
+    }
+    else
+    {
+        Console.WriteLine("0");
+    }
+}
+else
+{
+    Console.WriteLine("0");
+}
