a7d9e35fadc60e284348e1ce1eb9731a9deccb18
Алёна Чистякова <113229355+aspohdl@users.noreply.github.com>
2023-02-15 22:58:38 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+"""1"""
+# number = int(input())
+# if (number >= 10) and (number <= 99) and (number % 2 == 0):
+#     print("True")
+# else:
+#     print("False")
+
+"""2"""
+# number = int(input())
+# if number > 999:
+#     if (number % 10 == number / 1000) and (number % 100 / 10 == number % 1000 / 100):
+#         print("True")
+# else:
+#     print("False")
+
+"""3"""
+# x = float(input())
+# y = float(input())
+# if y > 0:
+#     if ((x ** 2 + y ** 2) <= 4) and ((x ** 2 + y ** 2) >= 1):
+#         print("1")
+#     else:
+#         print("0")
+# else:
+#     print("0")
+
+"""4"""
